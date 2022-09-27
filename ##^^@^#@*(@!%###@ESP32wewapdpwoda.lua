9fc56c6176c739f824f1d2dc4acc98be7a40b6d8
Notkonnerr32 <110717359+Notkonnerr32@users.noreply.github.com>
2022-09-26 23:04:13 -0500

    Create ##^^@^#@*(@!%###@ESP32wewapdpwoda.lua

---
+++ ##^^@^#@*(@!%###@ESP32wewapdpwoda.lua
@@ -0,0 +1,11 @@
+local players = game.Players:GetPlayers()
+
+for i,v in pairs(players) do
+ local esp = Instance.new("Highlight")
+ esp.Name = v.Name
+ esp.FillTransparency = 0
+ esp.FillColor = Color3.new(255, 0, 0)
+ esp.OutlineColor = Color3.new(25, 0, 0)
+ esp.OutlineTransparency = 0
+ esp.Parent = v.Character
+end
