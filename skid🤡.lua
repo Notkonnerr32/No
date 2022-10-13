921ed2048339f88ddbbbb01ab718e36329ee19cf
Notkonnerr32 <110717359+Notkonnerr32@users.noreply.github.com>
2022-10-12 22:53:27 -0500

    Create skid🤡.lua

---
+++ skid🤡.lua
@@ -0,0 +1,51 @@
+while true do
+
+wait()
+        game.Lighting.ClockTime = 24
+        wait()
+        game.Lighting.ClockTime = 23
+        wait()
+        game.Lighting.ClockTime = 22
+        wait()
+        game.Lighting.ClockTime = 21
+        wait()
+        game.Lighting.ClockTime = 20
+        wait()
+        game.Lighting.ClockTime = 19
+        wait()
+        game.Lighting.ClockTime = 18
+        wait()
+        game.Lighting.ClockTime = 17
+        wait()
+        game.Lighting.ClockTime = 16
+        wait()
+        game.Lighting.ClockTime = 15
+        wait()
+        game.Lighting.ClockTime = 14
+        wait()
+        game.Lighting.ClockTime = 13
+        wait()
+        game.Lighting.ClockTime = 12
+        wait()
+        game.Lighting.ClockTime = 11
+        wait()
+        game.Lighting.ClockTime = 10
+        wait()
+        game.Lighting.ClockTime = 9
+        wait()
+        game.Lighting.ClockTime = 8
+        wait()
+        game.Lighting.ClockTime = 7
+        wait()
+        game.Lighting.ClockTime = 6
+        wait()
+        game.Lighting.ClockTime = 5
+        wait()
+        game.Lighting.ClockTime = 4
+        wait()
+        game.Lighting.ClockTime = 3
+        wait()
+        game.Lighting.ClockTime = 2
+        wait()
+        game.Lighting.ClockTime = 1
+end
