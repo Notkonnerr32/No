ef6a2b11291a159d2dea9d06186921d6c24834e0
Notkonnerr32 <110717359+Notkonnerr32@users.noreply.github.com>
2022-10-14 08:21:30 -0500

    Update get fucked.lua

--- get fucked.lua
+++ get fucked.lua
@@ -1,1 +1,401 @@
-
+    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0    while _G.War == true do
+            War = Instance.new('Sound')
+            War.Parent = workspace
+            War.Looped = true
+            War.Name = "get fucked #SaturnHub"
+            War.Playing = true
+            War.SoundId = "rbxassetid://259174997"
+            War.Volume = 10
+            War.TimePosition = 0
