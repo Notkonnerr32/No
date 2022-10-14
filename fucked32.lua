110d49767fb36c5458a31685f1fcd0ec4d5bf510
Notkonnerr32 <110717359+Notkonnerr32@users.noreply.github.com>
2022-10-14 08:45:26 -0500

    Create fucked32.lua

---
+++ fucked32.lua
@@ -0,0 +1,9 @@
+ while true do
+ wait()
+local args = {
+    [1] = "GET NULLED XD #SaturnHub",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+end
